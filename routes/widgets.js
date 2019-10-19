f595fe30f10d78d51617679b842281fde92059d7
Logan Roberts <lroberts77@gmail.com>
2019-10-19 21:16:18 +0000

    Altered widgets route to use articles instead

--- routes/widgets.js
+++ routes/widgets.js
@@ -10,12 +10,12 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
+    let query = `SELECT * FROM articles`;
     console.log(query);
     db.query(query)
       .then(data => {
         const widgets = data.rows;
-        res.json({ widgets });
+        res.json({ articles });
       })
       .catch(err => {
         res
