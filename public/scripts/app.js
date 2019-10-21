f991c42f11e235a4d1aa78b894ff2566473e282b
arianrah <arianrahimi@icloud.com>
2019-10-21 22:37:18 +0000

    Cleaned app.js file, moved function specific code to their own js file.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
-
   $(() => {
     $.ajax({
       method: "GET",
       url: "/users"
     }).done((users) => {
+      users = users.users
       for(user of users) {
         $("<div>").text(user.name).appendTo($("body"));
       }
